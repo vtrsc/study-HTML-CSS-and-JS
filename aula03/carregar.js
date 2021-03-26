564915e959f7d9c0b6b88582bc7e63848269dd50
vsalaza <67997056+vsalaza@users.noreply.github.com>
2021-03-26 15:21:31 -0300

    add more things

--- aula03/carregar.js
+++ aula03/carregar.js
@@ -2,17 +2,18 @@ function carregar(){
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('imagem');
     var data = new Date();
-    var hora = data.getHours();
+    var hora = data.getHours() ;
+    var min = data.getMinutes();
 
-    msg.innerHTML = "agora sao" + hora + "horas"
+    msg.innerHTML = "agora sao " + hora +   " horas" + " e " + min + " minutos";
     if (hora >= 0 && hora < 12) {
         //bom dia
-          img.src = "fotomanha.jpg"
+          img.src = "fotomanha.jpg";
     }else if (hora >= 12 && hora < 18) {
         //boa tarde
-         img.src = 'fototarde.jpg'
+         img.src = 'fototarde.jpg';
     } else {
         // boa noite
-         img.src = 'fotonoite.jpg'
+         img.src = 'fotonoite.jpg';
     }
 }
