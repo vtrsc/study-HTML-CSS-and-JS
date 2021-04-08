8161aa175d07353db7fdd05d03af88ec85df3d46
vsalaza <67997056+vsalaza@users.noreply.github.com>
2021-04-08 19:31:31 -0300

    verificador de idade
    iinicializando o projeto de verificador de idades

---
+++ verificador_de_idade/script.js
@@ -0,0 +1,19 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getfullyear()
+    var fano = document.getElementById("txtano")
+    var res = document.querySelector("div#res")
+
+if(fano.value.length == 0 || fano.value > ano){
+    window.alert("verifique os dados")
+}else {
+    var fsex = document.getElementsByName("radsex")
+    var idade = ano - Number(fano.value)    
+   var genero = ""
+   if(fsex[0].checked){
+    genero = "homen"
+   }else if (fsex[1].checked){
+       genero = "mulher"
+   }
+   res.innerHTML = "detectamos "  +genero + "com" + idade + "anos";
+}
